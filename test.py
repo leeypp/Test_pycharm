d2efa593724a92a9a75a9b800392199e5691b215
leeypp <leeypp@foxmail.com>
2016-07-20 21:32:27 +0800

    change

--- test.py
+++ test.py
@@ -4,4 +4,19 @@ __author__ = 'leeypp'
 
 
 print "hello world!"
+sum = n = 1
+
+while n<=2:
+    print n
+    print "time is over",n
+    print n
+    n=1.0325*n
+    sum=sum+1
+print sum
+year = 0
+mon = 10000
+while mon < 20000:
+     mon = mon + mon*0.0325
+     year =year + 1
+print year
 
