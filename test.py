0f890880ab2e299e4e9f84fa4ddc6f373bbd488c
leeypp <leeypp@foxmail.com>
2016-07-20 21:52:35 +0800

    change

--- test.py
+++ test.py
@@ -5,18 +5,25 @@ __author__ = 'leeypp'
 
 print "hello world!"
 sum = n = 1
-
-while n<=2:
+name = ""
+sum1 = i = 0
+while n <= 2:
     print n
-    print "time is over",n
+    print "time is over", n
     print n
-    n=1.0325*n
-    sum=sum+1
+    n = 1.0325 * n
+    sum = sum + 1
 print sum
 year = 0
 mon = 10000
 while mon < 20000:
-     mon = mon + mon*0.0325
-     year =year + 1
+    mon = mon + mon * 0.0325
+    year = year + 1
 print year
+while name != " ":
+    name = raw_input("please input your number")
+    name = int(name)
+    i = i + 1
+    sum1 = sum1 + name
+    print "最终的结果是：", sum1 * 1.0 / i
 
