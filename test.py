4648fea7537111be2c7ee52ea23f429b77890ef0
leeypp <leeypp@foxmail.com>
2016-07-21 10:46:00 +0800

    change

--- test.py
+++ test.py
@@ -5,8 +5,9 @@ __author__ = 'leeypp'
 
 print "hello world!"
 sum = n = 1
-name = ""
+name = " "
 sum1 = i = 0
+# 两种方法计算汇率，本金10000，汇率3.25%，何时翻番
 while n <= 2:
     print n
     print "time is over", n
@@ -20,11 +21,11 @@ while mon < 20000:
     mon = mon + mon * 0.0325
     year = year + 1
 print year
-while name != " ":
+while name != "":  # 输入数字，当直接按回车的时候循环退出并打印结果
     name = raw_input("please input your number")
-    name = int(name)
-    i = i + 1
-    sum1 = sum1 + name
-    print "最终的结果是：", sum1 * 1.0 / i
-    print "this test file"
-    print "nothing"
+    if name!='':
+        name = int(name)
+        i = i + 1
+        sum1 = sum1 + name
+print "最终的结果是：", sum1 * 1.0 / i
+print "sucess"
