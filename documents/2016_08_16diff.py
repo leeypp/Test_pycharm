e6ba72b3c579f80d89dff7b0ab0d0dd0deebcb77
leeypp <leeypp@foxmail.com>
2016-08-17 10:13:01 +0800

    对比两个文件的差异，生成html文件，增加可读性

--- documents/2016_08_16diff.py
+++ documents/2016_08_16diff.py
@@ -20,3 +20,4 @@ text_lines2 = text2.splitlines()
 d = difflib.HtmlDiff()
 print d.make_file(text_lines1, text_lines2)
 # 采用HtmlDiff（）类的make_file（）方法将对比后的结果生成美观的HTML文档，增加可读性
+# python diff.py > diff.html
